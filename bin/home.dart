93f45cae60fa5f8e52b9b110fb74bc7334f12812
dell <omarmohamadhassan787@gmail.com>
2023-12-16 02:25:46 +0200

    Initial commit

---
+++ bin/home.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'dart:math';
+void main() {
+// task1();
+  sum(10, 20, 30, 40);
+}
+// task1() {
+//     int sum = 0;
+//
+//     for (int i = 2; i <= 50; i++) {
+//       sum += i;
+//     }
+//
+//     print("the sum is $sum");
+//   }
+
+void sum(int x, int y, int z, int q, [int w = 0]) {
+  print(x + y + z + q + w);
+}
+
+task3(){
+  print("enter the word");
+
+
+}
+
+
+
---
+++ test/home_test.dart
@@ -0,0 +1,8 @@
+import 'package:home/home.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
